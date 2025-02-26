4b5498258f729a644eed71fc6f79f1d3e33debfb
DerianCordobaPerez <derianricardo451@gmail.com>
2025-02-26 13:41:24 -0600

    chore(lib): removed the extra path to find the xcframework

--- lib/core/globals/globals.rb
+++ lib/core/globals/globals.rb
@@ -45,8 +45,7 @@ module SecureKeys
     # Returns the secure keys XCFramework path
     # @return [String] secure keys XCFramework path
     def secure_keys_xcframework_path
-      Dir.glob(["**/#{Swift::KEYS_DIRECTORY}/#{Swift::XCFRAMEWORK_DIRECTORY}",
-                "../**/#{Swift::KEYS_DIRECTORY}/#{Swift::XCFRAMEWORK_DIRECTORY}"]).first
+      Dir.glob("**/#{Swift::KEYS_DIRECTORY}/#{Swift::XCFRAMEWORK_DIRECTORY}").first
     end
 
     # Returns the supported iOS platforms
