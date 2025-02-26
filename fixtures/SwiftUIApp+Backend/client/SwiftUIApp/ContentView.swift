f1d8ecf14853d4cc0d3c6322c328049c7982c95b
DerianCordobaPerez <derianricardo451@gmail.com>
2025-02-26 16:11:46 -0600

    feat(fixture): created SwiftUI client app example project

---
+++ fixtures/SwiftUIApp+Backend/client/SwiftUIApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUIApp
+//
+//  Created by Derian Córdoba on 26/2/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ fixtures/SwiftUIApp+Backend/client/SwiftUIApp/SwiftUIAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIAppApp.swift
+//  SwiftUIApp
+//
+//  Created by Derian Córdoba on 26/2/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
