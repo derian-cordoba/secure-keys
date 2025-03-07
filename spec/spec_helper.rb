a553d558637076f3f50b8daa8bb320dcf604c4a9
DerianCordobaPerez <derianricardo451@gmail.com>
2025-03-06 22:54:58 -0600

    feat(tests): removed rspec output limit

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,3 +5,10 @@ require_relative './helpers/simplecov_env'
 require_relative './helpers/arguments/handler'
 
 SimpleCovEnv.start!
+
+RSpec.configure do |rspec|
+  rspec.expect_with :rspec do |config|
+    # Remove the max formatted output length
+    config.max_formatted_output_length = nil
+  end
+end
