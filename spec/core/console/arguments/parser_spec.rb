3ec02dd6ede14ba5a1f7978ffadc163aa713f042
DerianCordobaPerez <derianricardo451@gmail.com>
2025-03-10 20:15:02 -0600

    feat(tests): adjusted and added parser tests

--- spec/core/console/arguments/parser_spec.rb
+++ spec/core/console/arguments/parser_spec.rb
@@ -21,19 +21,40 @@ describe(SecureKeys::Core::Console::Argument::Parser) do
       'Usage: secure-keys [--options]',
       '',
       '-h, --help                       Use the provided commands to select the params',
-      '--add-xcframework-to-target TARGET',
-      'Add the xcframework to the target',
+      '--xcframework                Add the xcframework to the target',
       '-d, --delimiter DELIMITER        The delimiter to use for the key access (default: ",")',
+      '--[no-]generate              Generate the SecureKeys.xcframework',
       '-i, --identifier IDENTIFIER      The identifier to use for the key access (default: "secure-keys")',
       '--verbose                    Enable verbose mode (default: false)',
-      '-v, --version                    Show the secure-keys version',
-      '-x, --xcodeproj XCODEPROJ        The Xcode project path (default: the first found Xcode project)'
+      '-v, --version                    Show the secure-keys version'
     ]
 
     # when
     %w[-h --help].each do |option|
       output_lines = `./bin/secure-keys #{option}`.split("\n")
                                                   .map(&:strip)
+
+      # then
+      expect(output_lines).to(eq(expected_help))
+    end
+  end
+
+  it('should be the XCFramework help information from terminal output') do
+    # given
+    expected_help = [
+      'Usage: secure-keys --xcframework [--options]',
+      '',
+      '-h, --help                       Use the provided commands to select the params',
+      '--[no-]add                   Add the SecureKeys XCFramework to the Xcode project (default: true)',
+      '-t, --target TARGET              The target to add the xcframework',
+      '-r, --replace                    Replace the existing xcframework in the Xcode project (default: false)',
+      '-x, --xcodeproj XCODEPROJ        The Xcode project path (default: the first found Xcode project)'
+    ]
+
+    # when
+    %w[-h --help].each do |option|
+      output_lines = `./bin/secure-keys --xcframework #{option}`.split("\n")
+                                                                .map(&:strip)
 
       # then
       expect(output_lines).to(eq(expected_help))
