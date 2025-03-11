468ba18cd2289c0acd80e6c5f8966d2a52ead090
DerianCordobaPerez <derianricardo451@gmail.com>
2025-03-10 20:56:21 -0600

    refactor(lib): improved the `camelize` code to handle the ENV variables format

--- lib/core/utils/extensions/string/camelize.rb
+++ lib/core/utils/extensions/string/camelize.rb
@@ -3,13 +3,9 @@ module Camelize
   # Convert a string to camel case format
   # @return [String] the camel case formatted string
   def camelize
-    words = gsub(/[-_\s]+/, ' ') # Replace underscores, dashes, and spaces with a single space
-            .split(/(?=[A-Z])|\s+/) # Split the string into words while preserving uppercase transitions
-            .map(&:downcase) # Convert everything to lowercase to handle uppercase env variables
+    words = split(/(?<=[a-z])(?=[A-Z])|[-_\s]+/) # Split at lowercase-to-uppercase transitions or explicit separators
+            .map(&:downcase) # Convert everything to lowercase for consistency
 
-    # Lowercase the first word and capitalize the rest
-    words.map.with_index do |word, index|
-      index.zero? ? word : word.capitalize
-    end.join
+    words.map.with_index { |word, index| index.zero? ? word : word.capitalize }.join
   end
 end
